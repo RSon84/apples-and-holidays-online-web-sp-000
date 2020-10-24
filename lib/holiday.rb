062e3d3ae94df8b9ca76e6e9d4e42c099fffc9b4
Devin Wilson <craigvin84@yahoo.com>
2020-10-24 21:37:24 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,9 +75,8 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 holiday_hash.collect do |season, holiday|
-  holiday.collect do |supplies|
-    if supplies == "bbq"
-      holiday
+  holiday.collect do |holiday, supplies|
+  holiday  if supplies == "bbq"
 end
 end
 end
